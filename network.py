812954b4f6843225fb48634ec620b672919c6657
Justin Wasserman <jbwasse2@illinois.edu>
2019-12-29 12:34:05 -0600

    Networks (#5)
    -Added CNN and SNN, will need to play around with parameters later.

---
+++ network.py
@@ -0,0 +1,112 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import nengo
+import nengo_loihi
+import tensorflow as tf
+import nengo_dl
+import matplotlib.pyplot as plt
+from nengo.utils.filter_design import cont2discrete
+#Set seeds
+seed = 0
+tf.random.set_seed(seed)
+np.random.seed(seed)
+rng = np.random.RandomState(seed)
+
+#Create Conv Network (Just copy VGG16 or Resnet idc)
+model = torch.hub.load('pytorch/vision:v0.4.2', 'resnet101', pretrained=True)
+for param in model.parameters():
+    param.requires_grad = False
+    # Replace the last fully-connected layer
+    # Parameters of newly constructed modules have requires_grad=True by default
+model.fc = nn.Linear(2048, 101)
+
+#Create LMU cell
+#See https://www.nengo.ai/nengo-dl/examples/lmu.html
+class LMUCell(nengo.Network):
+    def __init__(self, units, order, theta, input_d, **kwargs):
+        super().__init__(**kwargs)
+
+        # compute the A and B matrices according to the LMU's mathematical derivation
+        # (see the paper for details)
+        Q = np.arange(order, dtype=np.float64)
+        R = (2 * Q + 1)[:, None] / theta
+        j, i = np.meshgrid(Q, Q)
+
+        A = np.where(i < j, -1, (-1.0) ** (i - j + 1)) * R
+        B = (-1.0) ** Q[:, None] * R
+        C = np.ones((1, order))
+        D = np.zeros((1,))
+
+        A, B, _, _, _ = cont2discrete((A, B, C, D), dt=1.0, method="zoh")
+
+        with self:
+            nengo_dl.configure_settings(trainable=None)
+
+            # create objects corresponding to the x/u/m/h variables in the above diagram
+            self.x = nengo.Node(size_in=input_d)
+            self.u = nengo.Node(size_in=1)
+            self.m = nengo.Node(size_in=order)
+            self.h = nengo_dl.TensorNode(tf.nn.tanh, shape_in=(units,), pass_time=False)
+
+            # compute u_t from the above diagram.
+            # note that setting synapse=0 (versus synapse=None) adds a one-timestep
+            # delay, so we can think of any connections with synapse=0 as representing
+            # value_{t-1}
+            nengo.Connection(
+                self.x, self.u, transform=np.ones((1, input_d)), synapse=None)
+            nengo.Connection(self.h, self.u, transform=np.zeros((1, units)), synapse=0)
+            nengo.Connection(self.m, self.u, transform=np.zeros((1, order)), synapse=0)
+
+            # compute m_t
+            # in this implementation we'll make A and B non-trainable, but they
+            # could also be optimized in the same way as the other parameters
+            conn_A = nengo.Connection(self.m, self.m, transform=A, synapse=0)
+            self.config[conn_A].trainable = False
+            conn_B = nengo.Connection(self.u, self.m, transform=B, synapse=None)
+            self.config[conn_B].trainable = False
+
+            # compute h_t
+            nengo.Connection(
+                self.x, self.h, transform=np.zeros((units, input_d)), synapse=None
+            )
+            nengo.Connection(
+                self.h, self.h, transform=np.zeros((units, units)), synapse=0)
+            nengo.Connection(
+                self.m,
+                self.h,
+                transform=nengo_dl.dists.Glorot(distribution="normal"),
+                synapse=None,
+            )
+
+#Create SNN 
+def build_model():
+    with nengo.Network(seed=seed) as net:
+        # remove some unnecessary features to speed up the training
+        nengo_dl.configure_settings(
+            trainable=None, stateful=False, keep_history=False,
+        )
+
+        # input node
+        inp = nengo.Node(np.zeros(train_images.shape[-1]))
+
+        # lmu cell
+        lmu = LMUCell(
+            units=212,
+            order=256,
+            theta=train_images.shape[1],
+            input_d=train_images.shape[-1]
+        )
+        conn = nengo.Connection(inp, lmu.x, synapse=None)
+        net.config[conn].trainable = False
+
+        # dense linear readout
+        out = nengo.Node(size_in=101)
+        nengo.Connection(lmu.h, out, transform=nengo_dl.dists.Glorot(), synapse=None)
+
+        # record output. note that we set keep_history=False above, so this will
+        # only record the output on the last timestep (which is all we need
+        # on this task)
+        p = nengo.Probe(out)
+
+SNN = build_model()
