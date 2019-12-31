9b608de9ca2ce7fa1605a66f70ef60a54143d9e4
Justin W <jbwasse2@illinois.edu>
2019-12-31 12:15:04 -0600

    Managed to load pretrained models

--- main.py
+++ main.py
@@ -6,128 +6,47 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from tensorboardX import SummaryWriter
 
-from dataloader import get_classes, create_dataloaders, get_image_size
+#from dataloader import get_classes, create_dataloaders, get_image_size
 from network import build_CNN, build_SNN
 from utils import get_args, setup
 from logger import Logger
+from functions import ResCNNEncoder,DecoderRNN
 logger = Logger('/home/justin/data/logs/action/')
 args = get_args()
 device, kwargs = setup(args)
-#Step 1 - Train CNN
-SNN = build_SNN(get_image_size(), args)
-CNN = build_CNN()
+#Step 1 - Get Pre-trained CNN
+#SNN = build_SNN(get_image_size(), args)
+# EncoderCNN architecture
+CNN_fc_hidden1, CNN_fc_hidden2 = 1024, 768
+CNN_embed_dim = 512   # latent dim extracted by 2D CNN
+res_size = 224        # ResNet image size
+dropout_p = 0.0       # dropout probability
 
-#https://github.com/victoresque/pytorch-template/blob/master/model/metric.py
-def get_accuracy(output, target):
-    with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
+# DecoderRNN architecture
+RNN_hidden_layers = 3
+RNN_hidden_nodes = 512
+RNN_FC_dim = 256
 
-def test(args, model, device, test_loader, epoch):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    accuracies = []
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(test_loader):
-            data, target = data.to(device), target.to(device)
-            data = data.squeeze()
-            #I have a gut feeling that the data shape could be wrong sometimes
-            if len(data.shape) != 4:
-                continue
-            output = model(data)
-            test_loss += F.cross_entropy(output, target)
-            accuracies.append(get_accuracy(output, target))
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-            if batch_idx % args.log_interval == 0:
-                step = epoch * len(test_loader) + batch_idx
-                print('Test Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(test_loader.dataset),
-                    100. * batch_idx / len(test_loader), test_loss.item()))
+# training parameters
+k = 101             # number of target category
+epochs = 120        # training epochs
+batch_size = 40
+learning_rate = 1e-3
+log_interval = 10   # interval for displaying training info
+CNN = ResCNNEncoder(fc_hidden1=CNN_fc_hidden1, fc_hidden2=CNN_fc_hidden2, drop_p=dropout_p, CNN_embed_dim=CNN_embed_dim).to(device)
+RNN = DecoderRNN(CNN_embed_dim=CNN_embed_dim, h_RNN_layers=RNN_hidden_layers, h_RNN=RNN_hidden_nodes, h_FC_dim=RNN_FC_dim, drop_p=dropout_p, num_classes=k).to(device)
+CNN_PATH = "/home/justin/data/models/action/pretrained/ResNetCRNN/cnn_encoder_epoch63_singleGPU.pth"
+RNN_PATH = "/home/justin/data/models/action/pretrained/ResNetCRNN/rnn_decoder_epoch63_singleGPU.pth"
+CNN.load_state_dict(torch.load(CNN_PATH))
+CNN.eval()
+RNN.load_state_dict(torch.load(RNN_PATH))
+RNN.eval()
 
-    accuracy = sum(accuracies)/ len(accuracies) 
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    print("accuracy2",accuracy)
-    # ================================================================== #
-    #                        Tensorboard Logging                         #
-    # ================================================================== #
-    #https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/04-utils/tensorboard/main.py
-
-    # 1. Log scalar values (scalar summary)
-    info = { 'test_loss': test_loss.item(), 'test_accuracy': accuracy }
-
-    for tag, value in info.items():
-        logger.scalar_summary(tag, value, epoch+1)
-
-    # 2. Log values and gradients of the parameters (histogram summary)
-    for tag, value in model.named_parameters():
-        tag = tag.replace('.', '/')
-        logger.histo_summary(tag, value.data.cpu().numpy(), epoch+1)
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    #Train_loader gets video, audio, label
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        data = data.squeeze()
-        #I have a gut feeling that the data shape could be wrong sometimes
-        if len(data.shape) != 4:
-            continue
-        output = model(data)
-        loss = F.cross_entropy(output, target)
-        loss.backward()
-        optimizer.step()
-        accuracy = get_accuracy(output, target)
-        if batch_idx % args.log_interval == 0:
-            step = epoch * len(train_loader) + batch_idx
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-
-            # ================================================================== #
-            #                        Tensorboard Logging                         #
-            # ================================================================== #
-            #https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/04-utils/tensorboard/main.py
-
-            # 1. Log scalar values (scalar summary)
-            info = { 'train_loss': loss.item(), 'train_accuracy': accuracy }
-
-            for tag, value in info.items():
-                logger.scalar_summary(tag, value, step+1)
-
-            # 2. Log values and gradients of the parameters (histogram summary)
-            for tag, value in model.named_parameters():
-                tag = tag.replace('.', '/')
-                logger.histo_summary(tag, value.data.cpu().numpy(), step+1)
-                #logger.histo_summary(tag+'/grad', value.grad.data.cpu().numpy(), batch_idx+1)
-
-            # 3. Log training images (image summary)
-        #    info = { 'images': images.view(-1, 28, 28)[:10].cpu().numpy() }
-
-#            for tag, images in info.items():
-#                logger.image_summary(tag, images, batch_idx+1)
+#Step 2 Attach RNN to CNN and get results to test everything works
 
 
-CNN = CNN.to(device)
-train_dataloader, test_dataloader = create_dataloaders(args)
-optimizer = optim.Adadelta(CNN.parameters(), lr=args.lr)
-scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
 
-for epoch in range(1, args.epochs + 1):
-    train(args, CNN, device, train_dataloader, optimizer, epoch)
-    test(args, CNN, device, test_dataloader, epoch)
-    scheduler.step()
 
-if args.save_model:
-    torch.save(CNN.state_dict(), "action_cnn.pt")
 
 #Step 2 - Remove FC layer of CNN
 
